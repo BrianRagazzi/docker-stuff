fc9a4d48713886bff4e2c52ec0aafe8f0a179e3b
Brian Ragazzi <brianpragazzi@gmail.com>
2019-09-06 08:32:04 -0700

    correct registry fqdn

--- windows/dotnet35/buildandtag.bat
+++ windows/dotnet35/buildandtag.bat
@@ -1,5 +1,5 @@
-#!/bin/bash
+
 docker build -t dotnet-helloworld:3.5 .
 
-docker tag dotnet-helloworld:3.5 harbor.ragazzilab.com/windows/dotnet-helloworld:3.5
-docker push harbor.ragazzilab.com/windows/dotnet-helloworld:3.5
+docker tag dotnet-helloworld:3.5 harbor.pae.ragazzilab.com/windows/dotnet-helloworld:3.5
+docker push harbor.pae.ragazzilab.com/windows/dotnet-helloworld:3.5
