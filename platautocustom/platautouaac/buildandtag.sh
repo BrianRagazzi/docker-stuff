8c354b4bb2e5c477e775ba05204574ad27ffc209
Brian Ragazzi <brianpragazzi@gmail.com>
2020-04-03 16:14:58 -0500

    updates

--- platautocustom/platautouaac/buildandtag.sh
+++ platautocustom/platautouaac/buildandtag.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build -t platautouaac:4.0.3 .
+docker build -t platautouaac:4.3.0 .
 
-docker tag platautouaac:4.0.3 harbor.ragazzilab.com/platauto/platautouaac:4.0.3
-docker push harbor.ragazzilab.com/platauto/platautouaac:4.0.3
+docker tag platautouaac:4.3.0 harbor.pae.ragazzilab.com/platauto/platautouaac:4.3.0
+docker push harbor.pae.ragazzilab.com/platauto/platautouaac:4.3.0
