764bb0547e336effdf1b3afe8bba589479b62430
Brian Ragazzi <brianpragazzi@gmail.com>
2020-10-11 10:14:14 -0500

    Update buildandtag.bat

--- windows/dotnet35/buildandtag.bat
+++ windows/dotnet35/buildandtag.bat
@@ -1,5 +1,7 @@
 
 docker build -t dotnet-helloworld:3.5 .
 
-docker tag dotnet-helloworld:3.5 harbor.pae.ragazzilab.com/windows/dotnet-helloworld:3.5
-docker push harbor.pae.ragazzilab.com/windows/dotnet-helloworld:3.5
+docker tag dotnet-helloworld:3.5 harbor.ragazzilab.com/windows/dotnet-helloworld:3.5
+docker push harbor.ragazzilab.com/windows/dotnet-helloworld:3.5
+
+docker tag dotnet-helloworld:3.5 ragazzilab/dotnet-helloworld:3.5
