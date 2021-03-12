b2dfd810cf8c96fd08bfcdcff89fa82142694cc6
Brian Ragazzi <brianpragazzi@gmail.com>
2021-03-12 09:34:15 -0600

    xmage

--- xmage/buildandtag.sh
+++ xmage/buildandtag.sh
@@ -6,7 +6,6 @@ docker build --build-arg XMAGE_VERSION=$VER -t docker-xmage-alpine:$VER .
 #
 docker tag docker-xmage-alpine:$VER harbor.ragazzilab.com/library/docker-xmage-alpine:$VER
 docker push harbor.ragazzilab.com/library/docker-xmage-alpine:$VER
-#docker rmi docker-xmage-alpine:latest
 docker rmi harbor.ragazzilab.com/library/docker-xmage-alpine:latest
 docker tag harbor.ragazzilab.com/library/docker-xmage-alpine:$VER harbor.ragazzilab.com/library/docker-xmage-alpine:latest
 docker push harbor.ragazzilab.com/library/docker-xmage-alpine:latest
